c5d4df9614f3bd48c53b1ddacbd0ba6c91c9bfa2
HassaneJer <40151330+hassanejer@users.noreply.github.com>
2022-10-25 21:51:27 +0100

    Read me : How to launch json-server

---
+++ src/BlogDetails.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import useFetch from './useFetch'
+
+function BlogDetails() {
+    const {id} = useParams()
+    const {data:blog, isLoading, error } = useFetch('http://localhost:8000/blogs/' + id)
+    const navigate = useNavigate()
+
+
+    function handleDelete(){
+      fetch('http://localhost:8000/blogs/' + id,{
+        method : 'DELETE'
+      }).then(()=> {
+        navigate('/')
+      })
+    }
+
+  return (
+    <div className='blog-details' >
+      {isLoading && <div>Loading</div> }
+      {error && <div>{error}</div> }
+      {
+        blog && (
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <div>
+                    {blog.body}
+                </div>
+
+                <button onClick={handleDelete}>Delete</button>
+            </article>
+        )
+      }
+    </div>
+  )
+}
+
+export default BlogDetails
---
+++ src/useFetch.jsx
@@ -0,0 +1,38 @@
+import {useEffect,useState} from 'react'
+
+function useFetch(url) {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+  
+  
+  
+    useEffect(()=>{
+  
+      setTimeout(()=>{
+        fetch(url)
+        .then(res =>{
+          if(!res.ok){
+            throw Error('Could Not Fetch The Data!!')
+          }
+          return res.json()
+        })
+        .then(data =>{
+            setData(data)
+            setIsLoading(false)
+            setError(null)
+        })
+        .catch((err)=>{
+            setError(err.message)
+            setIsLoading(false)
+          
+        })
+      },1000)
+      
+      
+    }, [url])
+
+    return {data, isLoading, error}
+}
+
+export default useFetch
